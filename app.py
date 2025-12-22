d5a6f9db420cf61b0df86d7a89598014b57f01ac
Kamaleshvar1 <gangatestmails@gmail.com>
2025-12-22 15:45:25 +0530

    Disable debug mode in Flask app
    Changed the Flask app's run configuration to set debug=False. This is likely for production deployment to prevent exposing debug information.

--- app.py
+++ app.py
@@ -1490,5 +1490,5 @@ def generate_pdf_content(quotation):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
